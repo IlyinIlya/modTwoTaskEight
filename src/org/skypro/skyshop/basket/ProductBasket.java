6a6bf0b27b3220da7a58acb94ab29b5c410f9fcb
grimm <ilyin88ok@gmail.com>
2026-08-27 19:00:57 +0330

    add project from mod2Task7

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,84 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductBasket {
+    // Basket contains a list of products
+    private final Map<String, List<Product>> products = new HashMap<>();
+
+    //Method for adding a product to the basket
+    public void addProduct(Product product) {
+        String nameProduct = product.getName();
+        products.computeIfAbsent(nameProduct, k -> new ArrayList<>()).add(product);
+    }
+
+    //Method for obtaining the total basket cost
+    public int getTotalBasketCost() {
+        int totalCost = 0;
+        for (List<Product> listProduct : products.values()) {
+            for (Product product : listProduct) {
+                if (product != null) {
+                    totalCost += product.getPrice();
+                }
+            }
+        }
+        return totalCost;
+    }
+
+    //Method counts special products
+    public int countSpecialProducts() {
+        int iCount = 0;
+        for (List<Product> listProduct : products.values()) {
+            for (Product product : listProduct) {
+                if (product != null && product.isSpecial()) {
+                    iCount++;
+                }
+            }
+        }
+        return iCount;
+    }
+
+    //Method prints the basket contents
+    public void printBasketContents() {
+        boolean emptybasket = true;
+        for (List<Product> listProduct : products.values()) {
+            for (Product product : listProduct) {
+                if (product != null) {
+                    System.out.println(product.toString());
+                    emptybasket = false;
+                }
+            }
+        }
+        if (emptybasket) {
+            System.out.println("В корзине пусто");
+        } else {
+            System.out.println("Итого: " + getTotalBasketCost());
+            System.out.println("Специальных товаров: " + countSpecialProducts());
+        }
+
+    }
+
+    //Method for checking a product in the basket by name
+    public boolean checkProductName(String productName) {
+        return products.containsKey(productName);
+    }
+
+    //Method for clearing the basket
+    public void clearBasket() {
+        products.clear();
+    }
+
+    public List<Product> basketRemoveProductByName(String name) {
+        List<Product> basketItemsRemove = products.remove(name);
+
+        if (basketItemsRemove == null) {
+            return new ArrayList<>();
+        }
+        return basketItemsRemove;
+    }
+}
---
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -0,0 +1,63 @@
+package org.skypro.skyshop.search;
+
+import org.skypro.skyshop.exceptions.BestResultNotFound;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SearchEngine {
+    private final List<Searchable> searchables;
+
+    public SearchEngine(int iSize) {
+        searchables = new LinkedList<>();
+    }
+
+    public void add(Searchable searchable) {
+        searchables.add(searchable);
+    }
+
+    public Map<String, Searchable> search(String searchTerm) {
+        Map<String, Searchable> elements = new TreeMap<>();
+        for (Searchable searchable : searchables) {
+            if (searchable != null && searchable.getSearchTerm().toLowerCase().contains(searchTerm.toLowerCase())) {
+                elements.put(searchable.getName(), searchable);
+            }
+        }
+        return elements;
+    }
+
+    public Searchable getMostEqualElement(String search) throws BestResultNotFound {
+        Searchable mostEqualElement = null;
+        int iMaxCount = 0;
+
+        for (Searchable searchable : searchables) {
+            if (searchable == null) {
+                throw new BestResultNotFound(search);
+            }
+
+            String iSearch = searchable.getSearchTerm();
+
+            int iCount = 0;
+            int index = 0;
+            int indexSubstring = iSearch.indexOf(search, index);
+
+            while (indexSubstring != -1) {
+                iCount++;
+                index = indexSubstring + search.length();
+                indexSubstring = iSearch.indexOf(search, index);
+            }
+
+            if (iCount > iMaxCount) {
+                iMaxCount = iCount;
+                mostEqualElement = searchable;
+            }
+        }
+        if (mostEqualElement == null) {
+            throw new BestResultNotFound(search);
+        }
+        return mostEqualElement;
+    }
+
+}
